5100d8d878a0242866d0d10d3c186d94f5d951fc
Mohamed Wahban <m.whban369@gmail.com>
2025-11-23 21:27:48 +0200

    Add Expect Min Max

--- ai_agent.py
+++ ai_agent.py
@@ -166,7 +166,7 @@ class MiniMaxAlphaBeta():
             min_eval = math.inf
             for col in board.get_valid_cols():
                 board.makemove(col, self.opp_player)
-                eval = self.minimax(board, True, depth - 1)
+                eval = self.minimax(board, False, alpha, beta, depth - 1)
                 board.undomove()
                 min_eval = min(min_eval, eval)
                 beta = min(min_eval, beta)
@@ -178,4 +178,92 @@ class MiniMaxAlphaBeta():
 
 
 class ExpectMinimax():
-    pass
+    def __init__(self, board_obj, k_depth, heuristic, ai_player, opp_player):
+        self.real_board = board_obj
+        self.k_depth = k_depth
+        self.heuristic = heuristic
+        self.ai_player = ai_player
+        self.opp_player = opp_player
+
+    def get_best_move(self):
+        simulation_board = self.real_board.copy()
+        best_eval = -math.inf
+        best_col = -1
+        valid_cols = simulation_board.get_valid_cols()
+
+        if len(valid_cols) == 1:
+            return valid_cols[0]
+
+        for col in valid_cols:
+            eval = self.calculate_chance_node(simulation_board, col, self.k_depth - 1, False)
+            
+            print(f"Col {col}: Expected Score {eval:.2f}")
+
+            if eval > best_eval:
+                best_eval = eval
+                best_col = col
+        
+        return best_col if best_col != -1 else valid_cols[0]
+
+    def expectiminimax(self, board_obj, depth, is_max):
+        if depth == 0 or board_obj.isterminal():
+            if board_obj.isterminal():
+                return board_obj.get_difference(self.ai_player, self.opp_player) * 100000
+            return self.heuristic.heuristic_evaluation(board_obj)
+
+        if is_max:
+            max_eval = -math.inf
+            for col in board_obj.get_valid_cols():
+                current_val = self.calculate_chance_node(board_obj, col, depth - 1, False)
+                max_eval = max(max_eval, current_val)
+            return max_eval
+        else:
+            min_eval = math.inf
+            for col in board_obj.get_valid_cols():
+                current_val = self.calculate_chance_node(board_obj, col, depth - 1, True)
+                min_eval = min(min_eval, current_val)
+            return min_eval
+
+    def calculate_chance_node(self, board_obj, intended_col, next_depth, next_is_max):
+        prob_intended = 0.6
+        prob_left = 0.2
+        prob_right = 0.2
+        
+        col_left = intended_col - 1
+        col_right = intended_col + 1
+        
+        if col_left < 0:
+            prob_intended += prob_left
+            prob_left = 0
+            
+        if col_right >= board_obj.cols:
+            prob_intended += prob_right
+            prob_right = 0
+
+        expected_value = 0
+        current_player = self.ai_player if not next_is_max else self.opp_player
+        
+        possible_moves = [
+            (intended_col, prob_intended),
+            (col_left, prob_left),
+            (col_right, prob_right)
+        ]
+
+        total_prob = 0
+
+        for col, prob in possible_moves:
+            if prob > 0:
+                if board_obj.isvalidmove(col):
+                    board_obj.makemove(col, current_player)
+                    val = self.expectiminimax(board_obj, next_depth, next_is_max)
+                    board_obj.undomove()
+                    
+                    expected_value += val * prob
+                    total_prob += prob
+                else:
+                    pass
+        
+        if total_prob > 0:
+            return expected_value / total_prob
+        else:
+            return -math.inf if not next_is_max else math.inf
